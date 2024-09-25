afaf94134f9cfde7967c494862d26d7c15f8dcf6
Adrián Fazanes Porto <adrifazanes38@gmail.com>
2024-09-25 13:27:37 +0200

    Ejercicio 14 Resuelto

--- src/main/java/com/campusdual/classroom/Doctor.java
+++ src/main/java/com/campusdual/classroom/Doctor.java
@@ -1,11 +1,16 @@
 package com.campusdual.classroom;
 
 public class Doctor extends Person {
-
 	protected String specialization;
 
 	public Doctor(String name, String surname, String specialization) {
 		super(name, surname);
 		this.specialization = specialization;
+	}
+
+	@Override
+	public void getDetails() {
+		super.getDetails();
+		System.out.println("Especialización: " + specialization);
 	}
 }
